299c95f789bd89994b72a555c199743cd706c22b
Gordlonious <gordlonious@gmail.com>
2018-03-22 02:38:49 -0600

    more comprehensive SearchNode proposal, will use as a game tree

--- SearchNode.java
+++ SearchNode.java
@@ -1,14 +1,41 @@
+
+import java.util.ArrayList;
+
 /**
  *
  * @author gordl
  */
 public class SearchNode implements Comparable<SearchNode> {
     private Integer priority;
-    Board board;
+    private Board board;
+    private SearchNode parent;
+    private ArrayList<SearchNode> children;
+    private boolean onGoalPath = false;
+    
+    // Board b is assumed to be the initial-board/root node
     public SearchNode(Board b, int moves) {
+        children = new ArrayList<>();
         board = b;
-        // Do both manhattan and hamming work here?
+        parent = null;
         priority = board.manhattan() + moves;
+    }
+    
+    // constructor for non-root search nodes
+    public SearchNode(Board b, int moves, SearchNode p) {
+        children = new ArrayList<>();
+        board = b;
+        parent = p;
+    }
+    
+    public void setChildren(ArrayList<Board> c, int moves, SearchNode p) {
+        for(Board b : c) {
+            SearchNode sn = new SearchNode(b, moves, p);
+            children.add(sn);
+        }
+    }
+    
+    public void flagAsLowestPriorityChild() {
+        onGoalPath = true;
     }
     
     @Override
--- Solver.java
+++ Solver.java
@@ -1,4 +1,4 @@
-
+import edu.princeton.cs.algs4.MinPQ;
 /**
  *
  * @author Gordon Portzline
@@ -8,4 +8,8 @@ public class Solver {
         if(b == null) throw new NullPointerException();
         if(!b.isSolvable()) throw new IllegalArgumentException("Inital board cannot be unsolvable");
     }
+    
+    public int moves() { throw new UnsupportedOperationException(); }
+    
+    public Iterable<Board> solution() { throw new UnsupportedOperationException(); }
 }
